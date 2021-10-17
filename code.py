a308b00894ebaa80da5f03cfbb07c65ec305f32c
bgdnkiller228 <86932863+bgdnkiller228@users.noreply.github.com>
2021-10-17 17:23:00 +0500

    Add files via upload

---
+++ code.py
@@ -0,0 +1,14 @@
+let = 'КЛНТЭ'
+s = []
+for a in let:
+    for b in let:
+        for c in let:
+            for d in let:
+                for e in let:
+                    for f in let:
+                        word = a + b + c + d + e + f
+                        s.append(word)
+for i in range(0, len(s)):
+    if s[i] == 'ККЛКЛК':
+        i += 1
+print(i)
